f4b7b50f1f293cd75106954fc729496fef5c1915
Omadzelevan <levanomadze13@gmail.com>
2024-10-19 00:22:43 +0400

    Add files via upload

---
+++ davaleba.py
@@ -0,0 +1,46 @@
+import math as m
+
+# 1
+# num = float(input("enter Number :"))
+#
+# if num > 0 :
+#     print("your number is Positive")
+# elif num < 0 :
+#     print("your number is Negative")
+# else :
+#     print("enter again the Number:")
+
+# 2 სხეულის მასის ინდექსი
+
+# weight = float(input("enter your weight :"))
+# height = float(input(("enter your Height :")))
+#
+# mass_index =  weight / (height ** 2)
+# if mass_index < 18.5:
+#     print("you have wonis deficiti")
+# elif  mass_index > 18.5 and mass_index < 24.9 :
+#     print("your weight is normal")
+# elif mass_index > 25 and mass_index < 29.9 :
+#     print("you have jarbi wona")
+# elif mass_index >30 and mass_index < 34.9 :
+#     print("shena gaqvs jigaro pirveli xarisxis simsuqne")
+# elif mass_index > 35 and mass_index < 39.9 :
+#     print("meore xarisxis simsuqne gaqvss")
+# elif mass_index >= 40:
+#     print("mesame xarisxis simsuqne  dagerxaa dd")
+# else :
+#     print("enter values")
+# print(f"your mass index is {mass_index}")
+
+
+# 3  შენი წელი ნაკიანია თუ არა
+
+# year = int(input("enter your birth year :"))
+#
+# if year % 4 == 0  and year % 100 != 0 :
+#     print(f"your year {year} is nakiani")
+# elif year % 400 == 0 :
+#     print(f" your year {year} is nakiani")
+# else :
+#     print(f"your year {year} is  not nakiani")
+
